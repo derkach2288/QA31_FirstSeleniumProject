6cde5230a7459f1e6dc3621d42afe99dc96fadd0
derkach2288 <derkach2288@gmail.com>
2024-02-27 21:47:45 +0100

    hw6

--- src/test/java/com/ait/qa31/CreateAccountTests.java
+++ src/test/java/com/ait/qa31/CreateAccountTests.java
@@ -1,38 +1,24 @@
 package com.ait.qa31;
 
-import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class CreateAccountTests extends TestBase{
 
+    @BeforeMethod
+    public void ensurePrecondition() {
+        if (!isRegisterLinkPresent()) {
+            clickOnLogOutButton();
+        }
+    }
+
     @Test
     public void createNewAccountPositiveTest() {
-
-        // click on Register link
-        click(By.cssSelector("[href='/register']"));
-
-        //enter First Name
-        type(By.id("FirstName"), "Ivan");
-
-        //enter Last Name
-        type(By.id("LastName"), "Ivanenko");
-
-        //enter email
-        type(By.id("Email"), "derkach@gmail.com");
-
-        //enter password
-        type(By.id("Password"), "Qwerty1234$");
-
-        //enter Confirm Password
-        type(By.id("ConfirmPassword"), "Qwerty1234$");
-
-        //click on the Registration button
-        click(By.id("register-button"));
-
-        // assert link with email is present
-        Assert.assertTrue(isElementPresent(By.xpath("//a[.='derkach@gmail.com']")));
-
+        clickOnRegisterLink();
+        fillRegisterForm("Ivan", "Ivanenko", "derkach@gmail.com", "Qwerty1234$");
+        clickOnRegisterButton();
+        Assert.assertTrue(isAccountCreatedByEmailLink());
     }
 
 }
--- src/test/java/com/ait/qa31/TestBase.java
+++ src/test/java/com/ait/qa31/TestBase.java
@@ -1,6 +1,7 @@
 package com.ait.qa31;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
@@ -36,5 +37,70 @@ public class TestBase {
 
     public void click(By locator) {
         driver.findElement(locator).click();
+    }
+
+    public void clickOnLogOutButton() {
+        click(By.cssSelector("[href='/logout']"));
+    }
+
+    public boolean isRegisterLinkPresent() {
+        return isElementPresent(By.cssSelector("[href='/register']"));
+    }
+
+    public boolean isAccountCreatedByEmailLink() {
+        return isElementPresent(By.xpath("//a[.='derkach@gmail.com']"));
+    }
+
+    public void clickOnRegisterButton() {
+        click(By.id("register-button"));
+    }
+
+    public void fillRegisterForm(String name, String lastName, String email, String password) {
+        type(By.id("FirstName"), name);
+        type(By.id("LastName"), lastName);
+        type(By.id("Email"), email);
+        type(By.id("Password"), password);
+        type(By.id("ConfirmPassword"), password);
+    }
+
+    public void clickOnRegisterLink() {
+        click(By.cssSelector("[href='/register']"));
+    }
+
+    public void clickOnLoginButton() {
+        click(By.cssSelector("[value='Log in']"));
+    }
+
+    public void fillLoginForm(String email, String password) {
+        type(By.cssSelector("#Email"), email);
+        type(By.cssSelector("#Password"), password);
+    }
+
+    public void clickOnLoginLink() {
+        click(By.cssSelector("[href='/login']"));
+    }
+
+    public boolean isLoginLinkPresent() {
+        return isElementPresent(By.cssSelector("[href='/login']"));
+    }
+
+    public boolean isProductAddedToCart() {
+        return isElementPresent(By.xpath("//td[@class='product']//a[.='14.1-inch Laptop']"));
+    }
+
+    public void clickOnShoppingCartLink() {
+        click(By.cssSelector(".ico-cart .cart-label"));
+    }
+
+    public void clickOnAddToCartButton() {
+        click(By.xpath("//div[@class='product-grid home-page-product-grid']/div[3]//input[@type='button']"));
+    }
+
+    public void deleteProductInCart() {
+        // click on the input quantity and enter "0"
+        type(By.cssSelector(".qty-input"), "0");
+
+        // press "Enter" key
+        driver.findElement(By.cssSelector(".qty-input")).sendKeys(Keys.ENTER);
     }
 }
